36ccd078106d5601de62c16d0dee65c97df8e889
Gideon Tettehfio <gideonlambride@gmail.com>
2024-05-03 00:56:14 +0000

    error handling bug fixed

--- weather_detector/weather_app/views.py
+++ weather_detector/weather_app/views.py
@@ -28,17 +28,32 @@ import urllib.request
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
-        res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=da63ed3d7fe23de14d3bcc4e2cbbb80c').read()
-        json_data = json.loads(res)
-        data = {
-            "country_code": str(json_data['sys']['country']),
-            "coordinate": str(json_data['coord']['lon']) + ' ' +
-            str(json_data['coord']['lat']),
-            "temp": str(json_data['main']['temp']) + 'k',
-            "pressure": str(json_data['main']['pressure']),
-            "humidity": str(json_data['main']['humidity']),
-            "weather_description": str(json_data['weather'][0]['description']),
-        }
+        if city:
+            try:
+                res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=da63ed3d7fe23de14d3bcc4e2cbbb80c').read()
+
+                json_data = json.loads(res)
+                data = {
+                "country_code": str(json_data['sys']['country']),
+                "coordinate": str(json_data['coord']['lon']) + ' ' +
+                str(json_data['coord']['lat']),
+                "temp": str(json_data['main']['temp']) + 'k',
+                "pressure": str(json_data['main']['pressure']),
+                "humidity": str(json_data['main']['humidity']),
+                "weather_description": str(json_data['weather'][0]['description']),
+                }
+
+            except urllib.error.HTTPError as e:
+                if e.code == 404:
+                    message = 'City was not found, please enter a valid city name'
+                    return render(request, 'index.html', {'city': city, 'message': message})
+                else:
+                    message = "An error occurred while processing your request."
+                    return render(request, 'index.html', {'city': city, 'message': message})
+
+        else:
+           message = "Please enter a city name."
+           return render(request, 'index.html', {'city': city, 'message': message})
     else:
         city = ''
         data = {}
